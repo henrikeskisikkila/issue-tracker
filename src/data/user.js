abd102a7b101d0f31679a450764e76e716ff4427
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-03-17 18:15:28 +0200

    Update user data

---
+++ src/data/user.js
@@ -0,0 +1,6 @@
+export const user = {
+  id: "61e54d6cbec7dc49106c8da6",
+  firstname: "John",
+  lastname: "Doe",
+  email: "john.doe@email.com",
+};
--- src/data/users.js
+++
@@ -1,8 +0,0 @@
-[
-  {
-    "id": "61e54d6cbec7dc49106c8da6",
-    "firstname": "John",
-    "lastname": "Doe",
-    "email": "john.doe@email.com"
-  }
-]
