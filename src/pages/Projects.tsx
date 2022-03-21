0c5ed904e0ca8b59f8463cc0b7050d2ea264c04a
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-03-21 17:14:34 +0200

    Update Project page to include header and menu

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { Header } from "components/Header";
+import { Navigator } from "components/Navigator";
+import { menu } from "config";
+import { projects } from "data/projects";
+
 export const Projects = () => {
-  return <>Projects</>;
+  const [newProject, setNewProject] = useState(true);
+
+  const showNewProject = () => {
+    return <>Add a new project</>;
+  };
+
+  const listProjects = () => {
+    return <>List of projects</>;
+  };
+
+  return (
+    <div>
+      <Header user={{ name: "User" }} onSearch={() => {}} onLogout={() => {}} />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Navigator menu={menu} />
+        {newProject ? listProjects() : showNewProject()}
+      </div>
+    </div>
+  );
 };
